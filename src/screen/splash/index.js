78209e430eec3c9b0b9303bc420e8710d09f22e9
Tiberlium <lonew047@gmail.com>
2021-12-02 12:48:47 +0700

    perbaikan data

--- src/screen/splash/index.js
+++ src/screen/splash/index.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { View,Image,StyleSheet } from 'react-native'
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Auth from '@react-native-firebase/auth';
 
 
 
-export default function Splash() {
+export default function Splash({navigation}) {
+    const [Initializing, setInitializing] = useState(true);
+    const [User, setUser] = useState();
+
+
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (Initializing) setInitializing(false);
+      }
+    
+
+      useEffect(() => {
+        const subscriber = Auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber;
+      }, [])
+      if (Initializing) return null;
+    
+      if(!User){
+        setTimeout(() => {
+          navigation.replace('Intro')
+        }, 2000);
+      }
+      else{
+        setTimeout(()=>{
+          navigation.replace('Navigator');
+        },2000)
+      }
+
+
     return (
         <View>
             <Image source={require('../../asset/Logo.png')} style={styles.img}/>
