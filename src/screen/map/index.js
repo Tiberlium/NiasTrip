ff1550773911e05f72bd85c308924a9a1e7dab1f
Tiberlium <lonew047@gmail.com>
2022-02-11 00:45:19 +0700

    perbaikan

--- src/screen/map/index.js
+++ src/screen/map/index.js
@@ -9,7 +9,28 @@ import firestore from '@react-native-firebase/firestore';
 
 import {Mapheadercard, Mapcard} from '../../component';
 
-import Icon from 'react-native-vector-icons/Feather';
+function nearby(lat1, lon1, lat2, lon2, unit) {
+  var radlat1 = (Math.PI * lat1) / 180;
+  var radlat2 = (Math.PI * lat2) / 180;
+  var theta = lon1 - lon2;
+  var radtheta = (Math.PI * theta) / 180;
+  var dist =
+    Math.sin(radlat1) * Math.sin(radlat2) +
+    Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  if (dist > 1) {
+    dist = 1;
+  }
+  dist = Math.acos(dist);
+  dist = (dist * 180) / Math.PI;
+  dist = dist * 60 * 1.1515;
+  if (unit == 'K') {
+    dist = dist * 1.609344;
+  }
+  if (unit == 'N') {
+    dist = dist * 0.8684;
+  }
+  return dist;
+}
 
 export default function Map({navigation, route}) {
   const [Wisata, setWisata] = useState([]);
@@ -20,8 +41,6 @@ export default function Map({navigation, route}) {
   const isMounted = useRef();
 
   const {id, latitude, longitude} = route.params;
-
-  const Pinloc = () => <Icon name="Map-pin" size={20} color="blue" />;
 
   async function getWisata() {
     let x = [];
@@ -96,12 +115,30 @@ export default function Map({navigation, route}) {
   }, []);
 
   let Data = [...Homestay, ...Event, ...Resto, ...Wisata];
+
+  const lat = Number(latitude);
+  const long = Number(longitude);
+
+  Data.map(doc => {
+    if (
+      nearby(
+        lat,
+        long,
+        Number(doc.data.Latitude),
+        Number(doc.data.Longitude),
+        'K',
+      ) <= 5
+    ) {
+      console.log(`${doc.data.Kategori}`);
+    }
+  });
+
   return (
     <>
       <View style={styles.container}>
         <Mapheadercard onPress={() => navigation.goBack()} />
         <MapView
-          initialRegion={{
+          region={{
             latitude: Number(latitude),
             longitude: Number(longitude),
             latitudeDelta: 0.0922,
