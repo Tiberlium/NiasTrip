30741697a20497a0a7dcad6b08c0c1ae8777f601
Tiberlium <lonew047@gmail.com>
2022-02-24 01:09:45 +0700

    perbaikan

--- src/screen/payment/index.js
+++ src/screen/payment/index.js
@@ -1,12 +1,5 @@
-import {
-  View,
-  Text,
-  Button,
-  StyleSheet,
-  SafeAreaView,
-  Dimensions,
-} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import React, {useEffect} from 'react';
 import axios from 'axios';
 import base64 from 'base-64';
 import {WebView} from 'react-native-webview';
@@ -15,52 +8,72 @@ const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').width;
 
 export default function Payment() {
-  // const [data, setdata] = React.useState({});
-  // const serverKey = 'SB-Mid-server-aOZTMq7MMpj0rwb4130chMv5:';
-  // const encodedKey = base64.encode(serverKey);
+  const [data, setdata] = React.useState({});
+  const serverKey = 'SB-Mid-server-aOZTMq7MMpj0rwb4130chMv5:';
+  const encodedKey = base64.encode(serverKey);
 
-  // const params = {
-  //   transaction_details: {
-  //     order_id: 'order-csb-2',
-  //     gross_amount: 10000,
-  //   },
-  //   credit_card: {
-  //     secure: true,
-  //   },
-  //   customer_details: {
-  //     first_name: 'Johny',
-  //     last_name: 'Kane',
-  //     email: 'testmidtrans@mailnesia.com',
-  //     phone: '08111222333',
-  //   },
-  // };
+  const params = {
+    transaction_details: {
+      order_id: 'order-csb-2',
+      gross_amount: 10000,
+    },
+    credit_card: {
+      secure: true,
+    },
+    customer_details: {
+      first_name: 'Johny',
+      last_name: 'Kane',
+      email: 'testmidtrans@mailnesia.com',
+      phone: '08111222333',
+    },
+  };
 
-  // axios({
-  //   url: 'https://app.sandbox.midtrans.com/snap/v1/transactions',
-  //   method: 'post',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Accept: 'application/json',
-  //     Authorization: 'Basic ' + encodedKey,
-  //   },
-  //   data: params,
-  // })
-  //   .then(snapResponse => {
-  //     let snapToken = snapResponse.data;
-  //     setdata(snapToken);
-  //   })
-  //   .catch(err => console.log(err));
+  function midtrans() {
+    axios({
+      url: 'https://app.sandbox.midtrans.com/snap/v1/transactions',
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: 'Basic ' + encodedKey,
+      },
+      data: params,
+    })
+      .then(snapResponse => {
+        let snapToken = snapResponse.data;
+        setdata(snapToken);
+      })
+      .catch(err => console.log(err));
+  }
+
+  function checkstatus() {
+    axios({
+      url: `https://api.sandbox.midtrans.com/v2/${params.transaction_details.order_id}/status`,
+      method: 'get',
+      headers: {
+        Accept: 'application/json',
+        'content-type': 'application/json',
+        Authorization: 'Basic ' + encodedKey,
+      },
+    })
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+  }
+
+  useEffect(() => {
+    midtrans();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <WebView source={{uri: 'https://infinite.red'}} />
+      <WebView source={{uri: data.redirect_url}} nestedScrollEnabled={true} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignContent:'center',
+    alignContent: 'center',
     height: height,
     width: width,
   },
