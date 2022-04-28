39072cb527315b30425dbe5fce8b3361d5a85015
Tiberlium <lonew047@gmail.com>
2022-04-28 23:33:45 +0700

    perbaikan

--- src/component/atom/Horizontalline.js
+++ src/component/atom/Horizontalline.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 export default function Horizontalline() {
   return (
-    <View style={{paddingHorizontal: 30}}>
+    <View style={{paddingHorizontal: 40}}>
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+        <View style={{flex: 1, height: 1, backgroundColor: 'grey'}} />
         <View>
           <Text style={{width: 50, textAlign: 'center'}}>Or</Text>
         </View>
-        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+        <View style={{flex: 1, height: 1, backgroundColor: 'grey'}} />
       </View>
     </View>
   );
