dc2e7204a2a677f6de10773bc72c66084fc2ee59
Tiberlium <lonew047@gmail.com>
2022-05-18 22:56:42 +0700

    perbaikan

--- src/component/template/Cardratingreview.js
+++ src/component/template/Cardratingreview.js
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
 import React from 'react';
 import {
   heightPercentageToDP as hp,
@@ -6,27 +6,45 @@ import {
 } from 'react-native-responsive-screen';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function Cardratingreview() {
+export default function Cardratingreview({onPress}) {
   return (
-    <View style={styles.container}>
+    <Pressable style={styles.container} onPress={onPress}>
+      <Image
+        source={require('../../asset/peopleicon.png')}
+        style={styles.img}
+      />
       <Text style={styles.txt}>Rating & Review</Text>
-    </View>
+      <Icon
+        name="chevron-forward-circle-outline"
+        size={25}
+        color="grey"
+        style={styles.icon}
+      />
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 10,
     height: hp(8),
     width: wp(90),
     alignSelf: 'center',
     margin: 5,
     borderRadius: 10,
     backgroundColor: '#D4E0F2',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: 10,
   },
+  img: {height: 40, width: 40},
   txt: {
-    color: 'black',
+    color: 'grey',
     fontWeight: '400',
-    textAlign: 'center',
-    marginTop: 18,
+    marginTop: 7,
+  },
+  icon: {
+    marginTop: 5,
   },
 });
