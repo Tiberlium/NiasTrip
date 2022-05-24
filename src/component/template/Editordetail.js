56d80f8cbcc3e58b28f04015ee3a1c6aa4d91fc5
Tiberlium <lonew047@gmail.com>
2022-05-25 02:05:39 +0700

    perbaikan

--- src/component/template/Editordetail.js
+++ src/component/template/Editordetail.js
@@ -2,29 +2,25 @@ import {View, Text, Image, Pressable, StyleSheet} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function Editordetail() {
+export default function Editordetail({url, title, desc}) {
   return (
     <Pressable style={styles.container}>
-      <View style={styles.inlineContainer}>
-        <Image
-          source={{uri: 'https://placeimg.com/640/480/nature'}}
-          style={styles.img}
-        />
-        <View>
-          <Text style={styles.title}>Villain peach</Text>
-          <View style={styles.wraplogo}>
-            <Icon name="flash" size={15} color="white" />
-            <Text style={styles.txticon}>By editor</Text>
+      <>
+        <View style={styles.inlineContainer}>
+          <Image source={{uri: url}} style={styles.img} />
+          <View>
+            <Text style={styles.title}>{title}</Text>
+            <View style={styles.wraplogo}>
+              <Icon name="flash" size={15} color="white" />
+              <Text style={styles.txticon}>By editor</Text>
+            </View>
           </View>
         </View>
-      </View>
-      <Text style={styles.subtitle}>Alasan Editor memilih ini :</Text>
-      <Text style={styles.caption}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.{' '}
-      </Text>
+      </>
+      <>
+        <Text style={styles.subtitle}>Alasan Editor memilih ini :</Text>
+        <Text style={styles.caption}>{desc}</Text>
+      </>
     </Pressable>
   );
 }
@@ -38,6 +34,8 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     borderRadius: 10,
     alignItems: 'baseline',
+    display: 'flex',
+    flexDirection: 'column',
   },
   img: {height: 100, width: 150, borderRadius: 10, margin: 10},
   title: {color: 'black', fontWeight: 'bold', fontSize: 18, marginTop: 10},
