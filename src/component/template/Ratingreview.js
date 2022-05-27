b5844ab54896c4aa26dcf96dd9dde4b7b24395c7
Tiberlium <lonew047@gmail.com>
2022-05-27 23:45:14 +0700

    perbaikan

--- src/component/template/Ratingreview.js
+++ src/component/template/Ratingreview.js
@@ -1,11 +1,19 @@
-import {Text, StyleSheet, Pressable, TextInput, View} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  View,
+  Image,
+} from 'react-native';
 import React from 'react';
 import ActionSheet from 'react-native-actions-sheet';
 import {Rating} from 'react-native-ratings';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function Ratingreview({refs}) {
-  return (
-    <ActionSheet ref={refs}>
+  const Postreview = () => (
+    <View>
       <Text style={styles.txt}>Berikan Ulasan anda</Text>
       <Rating showRating imageSize={30} />
       <View style={styles.input}>
@@ -18,9 +26,52 @@ export default function Ratingreview({refs}) {
       <Pressable style={styles.btn}>
         <Text style={styles.btntxt}>Posting</Text>
       </Pressable>
+    </View>
+  );
+
+  const Alterreview = () => (
+    <View>
+      <View style={styles2.wrap}>
+        <Image
+          source={{uri: 'https://placeimg.com/640/480/any'}}
+          style={styles2.img}
+        />
+        <View>
+          <Text style={styles2.txt}>Razor</Text>
+          <View style={styles2.inlineWrap}>
+            <Icon name="star" color="orange" size={20} />
+            <Text style={styles2.icontxt}>5</Text>
+          </View>
+        </View>
+      </View>
+      <Text style={styles2.caption}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </Text>
+    </View>
+  );
+  return (
+    <ActionSheet ref={refs}>
+      <Alterreview />
     </ActionSheet>
   );
 }
+
+const styles2 = StyleSheet.create({
+  wrap: {display: 'flex', flexDirection: 'row'},
+  img: {height: 70, width: 70, margin: 10, borderRadius: 40},
+  txt: {fontWeight: 'bold', color: 'black', fontSize: 18, marginTop: 15},
+  icontxt: {color: 'black', fontWeight: 'bold', marginLeft: 5},
+  inlineWrap: {display: 'flex', flexDirection: 'row', marginTop: 10},
+  caption: {
+    fontWeight: '400',
+    color: 'black',
+    width: 400,
+    marginHorizontal: 20,
+  },
+});
 
 const styles = StyleSheet.create({
   txt: {
