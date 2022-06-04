2c30e1d66f62129e888e7f648be4be77003a3584
Tiberlium <lonew047@gmail.com>
2022-06-04 23:41:13 +0700

    perbaikan

--- src/screen/receipt/index.js
+++ src/screen/receipt/index.js
@@ -1,7 +1,6 @@
 import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
-import ZigzagView from 'react-native-zigzag-view';
-import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function Receipt({route}) {
   const {guest, name, qty, checkin, checkout, timetransaction, total, orderId} =
@@ -9,96 +8,71 @@ export default function Receipt({route}) {
 
   return (
     <View>
-      <ZigzagView backgroundColor="#CCC" surfaceColor="#FFF">
+      <Icon
+        name="checkmark-circle"
+        size={30}
+        color="#3EAF0A"
+        style={styles.icon}
+      />
+      <Text style={styles.txt}>Pembayaran Berhasil</Text>
+      <Text style={styles.txtprice}>Rp100.000</Text>
+      <View style={styles.inlineContainer}>
         <Image source={require('../../asset/Logo.png')} style={styles.img} />
-        <Text style={styles.title}>Nias Trip</Text>
-        <Text style={styles.time}>{timetransaction}</Text>
+        <Text style={styles.txttitle}>Chipotle Grand Indonesia</Text>
         <View style={styles.wrap}>
-          <Text style={styles.nama}>Guest : </Text>
-          <Text style={styles.nama1}>{guest}</Text>
+          <Text style={styles.proptxt}>Nama Tamu</Text>
+          <Text style={styles.valuetxt}>Jarvis</Text>
         </View>
-        <Text style={styles.subhead}>Receipt</Text>
-        <Text style={styles.caption}>{name}</Text>
-        <View style={styles.wrap2}>
-          <Text style={styles.caption}>Jumlah</Text>
-          <Text style={styles.caption}>{qty} Orang</Text>
+        <View style={styles.wrap}>
+          <Text style={styles.proptxt}>Pembayaran</Text>
+          <Text style={styles.valuetxt}>Tempat Menginap</Text>
         </View>
-        <View style={styles.wrap2}>
-          <Text style={styles.caption}>Check In</Text>
-          <Text style={styles.caption}>{checkin}</Text>
-        </View>
-        <View style={styles.wrap2}>
-          <Text style={styles.caption}>Check Out</Text>
-          <Text style={styles.caption}>{checkout}</Text>
-        </View>
-        <View style={styles.wrap2}>
-          <Text style={styles.priceText}>Total</Text>
-          <Text style={styles.priceText}>Rp.{total}</Text>
-        </View>
-        <View style={styles.barcode}>
-          <QRCode value={orderId} size={120} />
-        </View>
-      </ZigzagView>
+      </View>
     </View>
   );
 }
 
-const styles = {
-  img: {
-    height: 100,
-    width: 100,
-    alignSelf: 'center',
-    marginTop: 30,
+const styles = StyleSheet.create({
+  icon: {alignSelf: 'center'},
+  txt: {
+    color: '#3EAF0A',
+    fontSize: 15,
+    marginTop: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
-  title: {
-    color: 'black',
+  txtprice: {
     fontSize: 30,
-    fontWeight: '500',
-    textAlign: 'center',
-  },
-  time: {
-    textAlign: 'center',
-  },
-  wrap: {display: 'flex', flexDirection: 'row'},
-  nama: {
-    marginTop: 20,
-    fontSize: 20,
     color: 'black',
-    marginLeft: 30,
-  },
-  nama1: {
-    marginTop: 20,
-    fontSize: 20,
-    color: 'black',
-    marginLeft: 10,
-  },
-  subhead: {
-    color: 'black',
-    fontSize: 25,
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 10,
   },
-  caption: {
+  img: {height: 50, width: 50, alignSelf: 'center'},
+  txttitle: {
+    fontWeight: 'bold',
+    fontSize: 18,
     color: 'black',
-    fontSize: 15,
-    textAlign: 'center',
-    marginTop: 10,
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 20,
   },
-  wrap2: {
+  inlineContainer: {
+    marginTop: 20,
+    width: 350,
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    elevation: 10,
+    borderRadius: 15,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+  wrap: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 30,
     marginTop: 10,
   },
-  priceText: {
-    fontSize: 20,
-    color: 'black',
-    marginTop: 10,
-  },
-  barcode: {
-    alignSelf: 'center',
-    padding: 30,
-  },
-};
+  proptxt: {color: 'grey', fontSize: 15, fontWeight: '500'},
+  valuetxt: {color: 'black', fontSize: 15, fontWeight: 'bold'},
+});
