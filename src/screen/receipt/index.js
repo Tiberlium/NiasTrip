50181041524632a45e9c5067e62f252d635aceb1
Tiberlium <lonew047@gmail.com>
2022-06-05 00:24:29 +0700

    perbaikan

--- src/screen/receipt/index.js
+++ src/screen/receipt/index.js
@@ -1,6 +1,12 @@
 import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {
+  Collapse,
+  CollapseHeader,
+  CollapseBody,
+} from 'accordion-collapse-react-native';
+import {Btnsubmit} from '../../component';
 
 export default function Receipt({route}) {
   const {guest, name, qty, checkin, checkout, timetransaction, total, orderId} =
@@ -27,7 +33,51 @@ export default function Receipt({route}) {
           <Text style={styles.proptxt}>Pembayaran</Text>
           <Text style={styles.valuetxt}>Tempat Menginap</Text>
         </View>
+        <View style={styles.wrap}>
+          <Text style={styles.proptxt}>Order id</Text>
+          <Text style={styles.valuetxt}>xdgax0842xxjdm</Text>
+        </View>
       </View>
+      <Collapse style={styles.collapse}>
+        <CollapseHeader>
+          <Text style={styles.transacttitletxt}>Detail Transaksi</Text>
+        </CollapseHeader>
+        <CollapseBody>
+          <View style={styles.wrap2}>
+            <Text style={styles.proptxt}>Metode Pembayaran</Text>
+            <Text style={styles.valuetxt}>Bca Klikpay</Text>
+          </View>
+          <View style={styles.wrap2}>
+            <Text style={styles.proptxt}>Status</Text>
+            <Text style={styles.valuetxt}>Selesai</Text>
+          </View>
+          <View style={styles.wrap2}>
+            <Text style={styles.proptxt}>Check in</Text>
+            <Text style={styles.valuetxt}>10/20/23</Text>
+          </View>
+          <View style={styles.wrap2}>
+            <Text style={styles.proptxt}>Check out</Text>
+            <Text style={styles.valuetxt}>10/20/23</Text>
+          </View>
+          <View style={styles.wrap2}>
+            <Text style={styles.proptxt}>Waktu</Text>
+            <Text style={styles.valuetxt}>20/20/2021 12:12:00</Text>
+          </View>
+          <View style={styles.wrap2}>
+            <Text style={styles.proptxt}>Jumlah orang</Text>
+            <Text style={styles.valuetxt}>4 orang</Text>
+          </View>
+          <View style={styles.wrap2}>
+            <Text style={styles.proptxt}>Biaya per malam</Text>
+            <Text style={styles.valuetxt}>Rp100.000</Text>
+          </View>
+           <View style={styles.wrap3}>
+            <Text style={styles.proptxt}>Total Pembayaran</Text>
+            <Text style={styles.valuetxt}>Rp200.000</Text>
+          </View>
+        </CollapseBody>
+      </Collapse>
+      <Btnsubmit title='Selesai' top={20}/>
     </View>
   );
 }
@@ -42,7 +92,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   txtprice: {
-    fontSize: 30,
+    fontSize: 25,
     color: 'black',
     fontWeight: 'bold',
     textAlign: 'center',
@@ -73,6 +123,32 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginTop: 10,
   },
-  proptxt: {color: 'grey', fontSize: 15, fontWeight: '500'},
-  valuetxt: {color: 'black', fontSize: 15, fontWeight: 'bold'},
+  wrap2: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    paddingHorizontal: 20,
+  },
+   wrap3: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  transacttitletxt: {
+    color: 'black',
+    marginLeft: 20,
+    fontWeight: 'bold',
+    fontSize: 17,
+  },
+  proptxt: {color: 'grey', fontSize: 13, fontWeight: '400'},
+  valuetxt: {color: 'black', fontSize: 13, fontWeight: 'bold'},
+  collapse: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    elevation: 10,
+    paddingVertical:20,
+  },
 });
