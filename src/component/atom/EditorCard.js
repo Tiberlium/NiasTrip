5fc0fb8073767e3fb13c9903098362ccc25f3e47
Tiberlium <lonew047@gmail.com>
2022-06-08 16:08:22 +0700

    perbaikan

--- src/component/atom/EditorCard.js
+++ src/component/atom/EditorCard.js
@@ -11,7 +11,7 @@ export default function EditorCard({children, onPress}) {
         style={styles.inlineContainer}>
         <View style={{display: 'flex', flexDirection: 'row'}}>
           <View style={styles.sideWrap}>
-            <Text style={styles.title}>By Editor</Text>
+            <Text style={styles.title}>Oleh Editor</Text>
             <Text style={{width: 120, marginTop: 10, color: 'white'}}>
               Berbagai tempat yang di rekomendasikan oleh editor
             </Text>
