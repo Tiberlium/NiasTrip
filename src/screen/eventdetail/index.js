5ca5ab44c9e3d3ac9e5f6506d310bf46ede317cf
Tiberlium <lonew047@gmail.com>
2022-06-25 14:17:15 +0700

    perbaikan

--- src/screen/eventdetail/index.js
+++ src/screen/eventdetail/index.js
@@ -74,6 +74,41 @@ export default function Eventdetail({navigation, route}) {
     }).format(uang);
   }
 
+  async function getrating() {
+    let x = [];
+
+    const docRef = await firestore()
+      .collection('Event')
+      .doc(id)
+      .collection('Comment')
+      .get();
+
+    const docRat = await firestore().collection('Event').doc(id);
+
+    docRef.docs.map(doc => {
+      doc.exists ? x.push({id: doc.id, data: doc.data()}) : [];
+    });
+
+    let _5star = x.filter(x => x.data.Rating === 5).length;
+    let _4star = x.filter(x => x.data.Rating === 4).length;
+    let _3star = x.filter(x => x.Rating === 3).length;
+    let _2star = x.filter(x => x.Rating === 2).length;
+    let _1star = x.filter(x => x.Rating === 1).length;
+
+    //jumlahkan bintang secara individual
+    let sumofrating = parseInt(_5star + _4star + _3star + _2star + _1star);
+
+    //jumlahkan total semua angka rating
+    let overalrating = parseInt(
+      5 * _5star + 4 * _4star + 3 * _3star + 2 * _2star + 1 * _1star,
+    );
+
+    //jumlahkan rata - rata
+    let average = parseInt(overalrating / sumofrating);
+
+    return docRat.update({Rating: average});
+  }
+
   async function Getyourcomment() {
     const docRef = await firestore()
       .collection('Event')
@@ -131,6 +166,12 @@ export default function Eventdetail({navigation, route}) {
     return () => (isMounted.current = false);
   }, []);
 
+  useEffect(() => {
+    isMounted.current = true;
+    getrating();
+    return () => (isMounted.current = false);
+  }, []);
+
   function addBookmark() {
     const value = {
       id: route.params.id,
@@ -168,6 +209,7 @@ export default function Eventdetail({navigation, route}) {
     ToastAndroid.show('Ulasan anda berhasil di post', ToastAndroid.SHORT);
     Getyourcomment();
     setisEdit(true);
+    getrating();
   }
 
   async function Editreview() {
