c94e080638ec6e45e7638945654a6dc074f013f5
Tiberlium <lonew047@gmail.com>
2022-07-18 04:39:52 +0700

    perbaikan

--- src/screen/coupon/index.js
+++ src/screen/coupon/index.js
@@ -1,15 +1,63 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
-import {Btnback,Promocard} from '../../component';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {Btnback, Promocard} from '../../component';
+import firestore from '@react-native-firebase/firestore';
 
 export default function Coupon({navigation}) {
+  const [data, setdata] = useState([]);
+
+  const ismounted = useRef();
+
+  async function get() {
+    let x = [];
+    const docRef = await firestore().collection('Promo').get();
+    docRef.docs.map(doc => {
+      x.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+
+    if (ismounted.current) return setdata(x);
+  }
+
+  useEffect(() => {
+    ismounted.current = true;
+    get();
+    return () => (ismounted.current = false);
+  }, []);
+
+  async function move(nama) {
+    let x = [];
+    const docRef = await firestore()
+      .collection('Staycation')
+      .where('Nama', '==', nama)
+      .get();
+
+    docRef.docs.map(doc => {
+      x.push({id: doc.id});
+    });
+
+    navigation.navigate('Hoteldetail', {id: x[0]['id']});
+  }
   return (
     <View>
       <View style={styles.inlineWrap}>
         <Btnback onPress={() => navigation.goBack()} />
         <Text style={styles.txt}>Promo</Text>
       </View>
-      <Promocard/>
+      <FlatList
+        data={data}
+        contentContainerStyle={{paddingBottom: 100}}
+        renderItem={({item, index}) => (
+          <Promocard
+            title={item['data']['Nama']}
+            discount={item['data']['Potongan']}
+            validdate={item['data']['Waktu']}
+            onpress={() => move(item['data']['Tempat'])}
+          />
+        )}
+      />
     </View>
   );
 }
